d8668b3a8cec2aab1436f74f3874335841968dec
perry <email@email.com>
2019-01-21 15:32:28 +0900

    もっと色々整理してみた

--- src/js/pages/SettingsPage.tsx
+++ src/js/pages/SettingsPage.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+<<<<<<< HEAD
 import { Link } from 'react-router-dom'
 
 interface IState {}
@@ -18,3 +19,15 @@ export default class SettingsPage extends React.Component<IProps, IState> {
     );
   }
 }
+=======
+import * as ReactDOM from 'react-dom';
+import App from './App';
+
+console.log('testing');
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+>>>>>>> 0761dbc... もっと色々整理してみた
--- src/js/pages/SplashPage.tsx
+++ src/js/pages/SplashPage.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+<<<<<<< HEAD
 import { Link } from 'react-router-dom'
 
 interface IState {
@@ -97,6 +98,100 @@ export default class SplashPage extends React.Component<IProps, IState> {
           <li><Link to='/settings/'>Settings</Link></li>
           <li><Link to='/stats/'>Stats</Link></li>
         </ul>
+=======
+// import logo from './logo.svg';
+// import './App.css';
+
+// I should put an interface for this app???
+
+interface IState {
+  userType: string,
+  firstName: string,
+  lastName: string,
+  middleName: string,
+  counter: number,
+  salary: number
+}
+
+interface IProps {
+
+}
+
+
+export default class App extends React.Component<IProps, IState> {
+
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      userType: null,
+      firstName: 'Whelp',
+      lastName: 'delp',
+      middleName: 'hawt',
+      counter: 0,
+      salary: null
+    };
+
+    this.handleOnClick = this.handleOnClick.bind(this);
+  }
+
+  handleOnClick(): void {
+    if (this.state.counter === 3) {
+      this.setState({
+        counter: 0
+      });
+    } else {
+      this.setState({
+        counter: this.state.counter + 1
+      });
+    }
+  }
+
+  showSomeJSX(): JSX.Element {
+    let famWord: string[] = ["Momma", "Papa", "Controller", "Soups"];
+    return (
+      <p>Hi {famWord[this.state.counter]}!!!</p>
+    );
+  }
+
+  showSomeOddJSX(): JSX.Element {
+    let famWord: string[] = ["Cool", "Alright", "GetUp", "GetDown"];
+    return (
+      <p>Hi {famWord[this.state.counter]}!!!</p>
+    );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <h1>{this.state.middleName}</h1>
+          <button id="pressMeButton" onClick={this.handleOnClick}>Counter</button>
+          <p>Current: {this.state.counter}</p>
+          <div>
+          Hmmm:
+          {
+            this.showSomeJSX()
+          }
+          </div>
+          <div>
+          Hmmm2:
+          {
+            this.showSomeOddJSX()
+          }
+          </div>
+        </header>
+>>>>>>> 0761dbc... もっと色々整理してみた
       </div>
     );
   }
