77add992b46bbc4ce9193c2aefd605e004baf90f
perry <email@email.com>
2019-02-02 19:49:44 +0900

    webpack設定を変更、distフォルダを追加、フォルダー構成を変更

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,30 @@
 const path = require('path');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+plugins: [
+    new CopyWebpackPlugin([
+        // Copy directory contents to {output}/to/directory/
+        { from: 'from/directory', to: 'to/directory' }
+        ])
+],
+
 
 module.exports = {
     entry: path.resolve(__dirname, './src/js/index.tsx'),
     output: {
-        path: path.resolve(__dirname, './public'),
+        path: path.resolve(__dirname, './dist/js'),
         filename: "perry_bundle.js",
         publicPath: '/'
     },
 
     //開発サーバーの設定
     devServer: {
-      contentBase: path.resolve(__dirname, './public'),
+      contentBase: path.resolve(__dirname, './dist'),
       inline: true,
       open: true
     },
 
     // Enable sourcemaps for debugging webpack's output.
     devtool: "source-map",
-
     resolve: {
         // Add '.ts' and '.tsx' as resolvable extensions.
         extensions: [".ts", ".tsx", ".js", ".json"]
@@ -33,9 +40,27 @@ module.exports = {
         ]
     },
     target: 'web',
-
-    // When importing a module whose path matches one of the following, just
-    // assume a corresponding global variable exists and use that instead.
-    // This is important because it allows us to avoid bundling all of our
-    // dependencies, which allows browsers to cache those libraries between builds.
+    plugins: [
+      new CopyWebpackPlugin([
+        // TODO: 以下のファイル設定あんまりよくない。
+        //       publicフォルダーに入ってるファイルをcopyするように修正しないといけない
+        {
+          from: path.resolve(__dirname, './public/favicon.ico'),
+          to: path.resolve(__dirname, 'dist'),
+          toType: 'dir'
+      },
+      {
+        from: path.resolve(__dirname, './public/manifest.json'),
+        to: path.resolve(__dirname, 'dist'),
+        toType: 'dir'
+      },
+      {
+        from: path.resolve(__dirname, './public/index.html'),
+        to: path.resolve(__dirname, 'dist'),
+        toType: 'dir'
+      }
+    ], {
+          copyUnmodified: true
+      })
+    ]
 };
